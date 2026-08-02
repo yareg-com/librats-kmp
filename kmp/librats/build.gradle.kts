38e071eef2067cfc6f2e6b3e93642f33f8bdd9c3
Yar Rick <yar@yareg.com>
2026-08-02 21:51:02 +0300

    Comment out JAVA_HOME

--- kmp/librats/build.gradle.kts
+++ kmp/librats/build.gradle.kts
@@ -71,7 +71,7 @@ val cmakeGenerate = tasks.register<Exec>("cmakeGenerate") {
 //        "-DRATS_SHARED_LIBRARY=ON",
 //        "-DRATS_STATIC_LIBRARY=OFF",
 //        "-DCMAKE_BUILD_TYPE=Release",
-        "-DJAVA_HOME=/home/yareg/.jdks/temurin-21.0.10/",
+//        "-DJAVA_HOME=",
         "-B", buildDir.absolutePath,
         "-S", sourceDir.absolutePath
     )
